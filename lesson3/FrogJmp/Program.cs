ce4c81284d8aaa3b62ff28128007b2887fad1063
Maycon Brito <mayconfsbrito@gmail.com>
2020-08-15 21:13:45 -0300

    Lesson 3.1

---
+++ lesson3/FrogJmp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace console.lesson2.CyclicRotation
+{
+    static class Solution
+    {
+        static void Main(string[] args)
+        {
+            var x = 10;
+            var y = 85;
+            var d = 30;
+            System.Console.WriteLine(solution(x, y, d));
+
+            x = 0; y = 5; d = 1;
+            System.Console.WriteLine(solution(x, y, d));
+
+            x = 10; y = 5; d = 30;
+            System.Console.WriteLine(solution(x, y, d));
+
+            x = 0; y = 1; d = 0;
+            System.Console.WriteLine(solution(x, y, d));
+
+            x = 12; y = 1; d = 9999999;
+            System.Console.WriteLine(solution(x, y, d));
+        }
+
+        public static int solution(int X, int Y, int D)
+        {
+            System.Console.WriteLine("Tentativa:");
+
+            if (X >= Y || D == 0) return 0;
+            return (int)Math.Ceiling((decimal)(Y - X) / D);
+
+        }
+    }
+}
